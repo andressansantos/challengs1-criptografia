340a785cc117fd23bf6c2524171c57e80865301b
Andressa Santos <san_100rock@yahoo.com.br>
2022-03-05 17:37:10 -0300

    atualização com script novo

--- js/script.js
+++ js/script.js
@@ -1,86 +1,65 @@
-
 function ativaBotao(){
-  //seleciona o imput
-  let texto = document.querySelector('#input-texto');
-  const result =document.querySelector('#input-texto');
+    var texto = document.getElementById('input-texto').value;
+    var textofinal = '';
+
   
+    if (texto.length == ""){
+        alert('Campo vazio ou inválido. Digite novamente');
+    }else{
+        //var textocript = document.getElementById('input-texto').value;
+        const regex = new RegExp("^[ 0-9a-zA-Z\b]+[A-Za-z0-9_\w]+$");
 
-  if (texto.value== ''){
-    alert("Campo vazio ou inválido. Digite novamente");
-  } 
+            if(!regex.test(texto)){
+                document.getElementById('input-texto').value='';
+                alert("Digite novamente");
+            }else{
+                textofinal = texto;
 
-  const regex = new RegExp("^[0-9a-zA-Z\b]+$");
-  var teste = false    
-
-  if(!regex.test(texto.value)){
-     teste = true
-     if(teste == true){
-        alert("Digite novamente");
-     }
-   }else{
-    cripto();
-    //console.log(cripto())
-
-   }
-}
-
-
-function cripto(){
-  let texto = document.querySelector('#texto')
-  let resultado = document.querySelector('#msg')
-  let textoValor = ''
-  //bloqueioTexto(texto.value.toLowerCase())
-  let textoResultante = textoValor
-    .replace(/[i\í]/gi, 'imes')
-    .replace(/[e\é\ê]/gi, 'enter')
-    .replace(/[a\á\â\ã]/gi, 'ai')
-    .replace(/[o\ó\ô]/gi, 'ober')
-    .replace(/[u\ú]/gi, 'ufat')
-  resultado.value = textoResultante
- // texto.value = ''
-  document.getElementById("msg").value = resultado;
- // alert(resultado)
-}
-
-
-
-function descriptarDados (){
-    let texto =  document.querySelector('#input-texto')
-
-    if (texto.value == ''){
-        alert("Campo vazio ou inválido. Digite novamente");
+            }
     }
-    navigator.clipboard.writeText(copia.value);
-    //return(copia);
-    alert("TEXTO COPIADO!");
-    limpa();
+
+       var troca = textofinal
+        .replace(/[e]/gi, 'enter')
+        .replace(/[i]/gi, 'imes')
+        .replace(/[a]/gi, 'ai')
+        .replace(/[o]/gi, 'ober')
+        .replace(/[u]/gi, 'ufat');
+        
+    document.getElementById("msg").value = troca;
 }
-
-
-/*
-function bloqueioTexto(texto){
-  //return texto.replace(/[1\\!\¹\'2'\@\²\'3'\#\³\'4'\$\£\'5'\%\¢\'6'\¨\¬\'7'\&\'8'\*\'9'\(\'0'\´\)\-\_\=\+\§\`\[\{\ª\~\^\}\º\|\,\<\.\>\;\:\\\\/\?\°\"\']/g, '')
-  var char = String.fromCharCode(texto.keyCode);
-  
-  console.log(char);
-    var pattern = '[a-zA-Z0-9]';
-    if (char.match(pattern)) {
-      return true;
-  }
-}
-
-*/
-
 
 function copiarTexto(){
 
-    let copiarTexto = document.querySelector('#msg')
+    let copiarTexto = document.querySelector("#msg");
+    copiarTexto.select();
+    copiarTexto.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    return copiarTexto
+}
 
+function descriptarDados(){
+    var textoDescrip = document.getElementById('input-texto').value;
 
-    /*
-    if(copiarTexto.value == ''){
+          
+    if (textoDescrip.length == ""){
+        alert('Campo vazio ou inválido. Digite novamente');
+    }else{
+        //var textocript = document.getElementById('input-texto').value;
+        const regex = new RegExp("^[ 0-9a-zA-Z\b]+[A-Za-z0-9_\w]+$");
 
+            if(!regex.test(textoDescrip)){
+                document.getElementById('input-texto').value='';
+                alert("Digite novamente");
+            }10;
+        //textofinal = texto.length;
     }
-*/
-return copiarTexto
+
+       var trocaDesc = textoDescrip
+        .replace(/enter/, 'e')
+        .replace(/ai/, 'a')
+        .replace(/imes/, 'i')
+        .replace(/ober/, 'o')
+        .replace(/ufat/, 'u');
+        
+    document.getElementById("msg").value = trocaDesc;
 }
